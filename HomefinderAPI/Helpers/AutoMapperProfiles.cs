55e7d11defcb8d7a769806c56efca6e56dc0c50b
Jonas <jonasstenbeerg@gmail.com>
2023-04-06 14:59:17 +0200

    added props to advertiesement viewmodel

--- HomefinderAPI/Helpers/AutoMapperProfiles.cs
+++ HomefinderAPI/Helpers/AutoMapperProfiles.cs
@@ -10,8 +10,14 @@ namespace HomefinderAPI.Helpers
         {
             //Från --> Till
             CreateMap<Advertisement,AdvertisementViewModel>()
-            .ForMember(dest => dest.TrackingNumber, options => options.MapFrom(src => src.Id))
-            .ForMember(dest => dest.ListPrice, options => options.MapFrom(src => src.Price));
+            .ForMember(dest => dest.SearchNumber, options => options.MapFrom(src => src.Id))
+            .ForMember(dest => dest.ListPrice, options => options.MapFrom(src => src.Price))
+            .ForMember(dest => dest.Area, options => options.MapFrom(src => src.Property.Area))
+            .ForMember(dest => dest.LeaseType, options => options.MapFrom(src => src.Property.LeaseType!.Name))
+            .ForMember(dest => dest.PropertyType, options => options.MapFrom(src => src.Property.PropertyType!.Name))
+            .ForMember(dest => dest.City, options => options.MapFrom(src => src.Property.Address!.City))
+            .ForMember(dest => dest.StreetAddress, options => options.MapFrom(src => src.Property.Address!.StreetAddress));
+            
         }
     }
 }
--- HomefinderAPI/ViewModels/AdvertisementViewModel.cs
+++ HomefinderAPI/ViewModels/AdvertisementViewModel.cs
@@ -2,8 +2,13 @@ namespace HomefinderAPI.ViewModels
 {
     public class AdvertisementViewModel
     {   
-        public int TrackingNumber { get; set; }
+        public int SearchNumber { get; set; }
         public int ListPrice { get; set; }
-        
+        public string? LeaseType { get; set; }
+        public string? PropertyType { get; set; }
+        public int Area { get; set; }
+        public string? City { get; set; }
+        public string? StreetAddress { get; set; }
+
     }
 }

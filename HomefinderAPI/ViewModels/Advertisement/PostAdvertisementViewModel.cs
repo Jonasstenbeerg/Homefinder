b78a6655f172639b197d12148c9802243acd0ae4
Jonas <jonasstenbeerg@gmail.com>
2023-04-09 17:19:35 +0200

    added errormessage for post model

--- HomefinderAPI/ViewModels/Advertisement/PostAdvertisementViewModel.cs
+++ HomefinderAPI/ViewModels/Advertisement/PostAdvertisementViewModel.cs
@@ -1,14 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HomefinderAPI.ViewModels.Advertisement
 {
 	public class PostAdvertisementViewModel
 	{
+		[Required(ErrorMessage = "Pris är obligatoriskt")]
 		public int ListPrice { get; set; }
+		[Required(ErrorMessage = "Arrendetyp är obligatoriskt")]
 		public string? LeaseType { get; set; }
+		[Required(ErrorMessage = "Objektstyp är obligatoriskt")]
 		public string? PropertyType { get; set; }
+		[Required(ErrorMessage = "Area är obligatoriskt")]
 		public int Area { get; set; }
+		[Required(ErrorMessage = "Stad är obligatoriskt")]
 		public string? City { get; set; }
 		public int PostalCode { get; set; }
+		[Required(ErrorMessage = "Gatunamn är obligatoriskt")]
 		public string? StreetName { get; set; }
+		[Required(ErrorMessage = "Gatunummer är obligatoriskt")]
 		public int StreetNumber { get; set; }
 	}
 }

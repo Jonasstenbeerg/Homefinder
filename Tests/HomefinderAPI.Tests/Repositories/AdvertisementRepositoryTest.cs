d4f8dbc3b7d23d002143f1408aaaf8d9ce6f6b87
Jonas <jonasstenbeerg@gmail.com>
2023-04-10 13:08:41 +0200

    added todo for repo tests

---
+++ Tests/HomefinderAPI.Tests/Repositories/AdvertisementRepositoryTest.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HomefinderAPI.Tests.Repositories
+{
+    [TestClass]
+    public class AdvertisementRepositoryTest
+    {
+        //TODO: Create tests for all repository methods
+    }
+}

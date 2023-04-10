ca6949476cee6efa5625eafb2e598cf64619a474
Jonas <jonasstenbeerg@gmail.com>
2023-04-10 13:09:39 +0200

    removed unused usings

--- Tests/HomefinderAPI.Tests/Repositories/AdvertisementRepositoryTest.cs
+++ Tests/HomefinderAPI.Tests/Repositories/AdvertisementRepositoryTest.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 namespace HomefinderAPI.Tests.Repositories
 {
-    [TestClass]
+  [TestClass]
     public class AdvertisementRepositoryTest
     {
         //TODO: Create tests for all repository methods

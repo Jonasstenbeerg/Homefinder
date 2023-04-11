4f98cbecb1c53f84cd8cb43f8cac773cc439ab16
Jonas <jonasstenbeerg@gmail.com>
2023-04-11 12:45:25 +0200

    added login method for AuthCntrl

--- HomefinderAPI/Controllers/AuthorizationController.cs
+++ HomefinderAPI/Controllers/AuthorizationController.cs
@@ -10,9 +10,11 @@ namespace HomefinderAPI.Controllers
 		public class AuthorizationController : ControllerBase
 		{
     private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
 		
-			public AuthorizationController(UserManager<IdentityUser> userManager)
+			public AuthorizationController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
 			{
+      _signInManager = signInManager;
       _userManager = userManager;
 					
 			}
@@ -50,6 +52,31 @@ namespace HomefinderAPI.Controllers
 					}
 					return StatusCode(500, ModelState);
 				}
+			}
+
+			[HttpPost("login")]
+			public async Task<ActionResult<UserViewModel>> Login(LoginUserViewModel model)
+			{
+				var user = await _userManager.FindByNameAsync(model.UserName!);
+
+				if (user is null)
+				{
+					return Unauthorized("Felaktigt användarnamn");
+				}
+
+				var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password!, true);
+
+				if (!result.Succeeded)
+				{
+					return Unauthorized(result.IsLockedOut ? "Kontot är låst i 10 minuter på grund av för många försök":"Felaktigt lösenord");
+				}
+
+				var userData = new UserViewModel
+				{
+					Username = user.UserName
+				};
+
+				return Ok(userData);
 			}
 		}
 }

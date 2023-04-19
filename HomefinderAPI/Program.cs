dcd343c0449c356253a4f25ae8577d1e4e94c611
Jonas <jonasstenbeerg@gmail.com>
2023-04-19 20:27:52 +0200

    jwt configured in program.cs

--- HomefinderAPI/Program.cs
+++ HomefinderAPI/Program.cs
@@ -1,9 +1,12 @@
+using System.Text;
 using HomefinderAPI.Data;
 using HomefinderAPI.Helpers;
 using HomefinderAPI.Interfaces;
 using HomefinderAPI.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,26 +14,48 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 // Context that uses SQLite
-builder.Services.AddDbContext<HomefinderContext>(options => {
+builder.Services.AddDbContext<HomefinderContext>(options => 
+{
 	options.UseSqlite(builder.Configuration.GetConnectionString("SQLite"));
 });
 
 //Configure Microsoft Identity for users
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(
-	options =>
-		{
-			options.Password.RequireDigit = true;
-			options.Password.RequireLowercase = true;
-			options.Password.RequireUppercase = true;
-			options.Password.RequireNonAlphanumeric = false;
-			options.Password.RequiredLength = 8;
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+	options.Password.RequireDigit = true;
+	options.Password.RequireLowercase = true;
+	options.Password.RequireUppercase = true;
+	options.Password.RequireNonAlphanumeric = false;
+	options.Password.RequiredLength = 8;
 
-			options.User.RequireUniqueEmail = true;
+	options.User.RequireUniqueEmail = true;
 
-			options.Lockout.MaxFailedAccessAttempts = 5;
-			options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
-		}
+	options.Lockout.MaxFailedAccessAttempts = 5;
+	options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+}
 ).AddEntityFrameworkStores<HomefinderContext>();
+
+//Configure Jason Web Token authentication
+builder.Services.AddAuthentication(options => 
+{
+	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+	options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options => 
+{
+	options.TokenValidationParameters = new TokenValidationParameters
+	{
+		ValidateIssuerSigningKey = true,
+		IssuerSigningKey = new SymmetricSecurityKey(
+				Encoding.ASCII.GetBytes(builder.Configuration.GetValue<string>("apiSecret")!)
+			),
+		ValidateLifetime = true,
+		ValidateAudience = false,
+		ValidateIssuer = false,
+		ClockSkew = TimeSpan.Zero
+	};
+});
 
 //Repo service for DI
 builder.Services.AddScoped<IAdvertisementRepository, AdvertisementRepository>();
@@ -56,6 +81,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();

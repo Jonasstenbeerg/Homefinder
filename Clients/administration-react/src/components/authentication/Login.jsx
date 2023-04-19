31e6a1959c6ca7b2335e2bd66afe89ed80dd39c4
Jonas <jonasstenbeerg@gmail.com>
2023-04-19 11:06:39 +0200

    updated nav size and fixed issues in login form

--- Clients/administration-react/src/components/authentication/Login.jsx
+++ Clients/administration-react/src/components/authentication/Login.jsx
@@ -4,11 +4,11 @@ const Login = () => {
     <form action="/login" method="post" className="form-container">
       <section className="form-props-area">
         <h1 className="form-heading">Sign in</h1>
-        <label className="form-label" for="form-username">Username:</label>
-        <input className="form-input" placeholder="Username" type="text" id="form-username" name="username" toComplete="off" required/>
+        <label className="form-label" htmlFor="form-username">Username:</label>
+        <input className="form-input" placeholder="Username" type="text" id="form-username" name="username" tocomplete="off" required/>
         
-        <label className="form-label" for="form-password">Password:</label>
-        <input className="form-input" placeholder="Password" type="password" id="form-password" name="password" toComplete="off" required/>
+        <label className="form-label" htmlFor="form-password">Password:</label>
+        <input className="form-input" placeholder="Password" type="password" id="form-password" name="password" tocomplete="off" required/>
         
         <a href="/forgot-password" id="form-forgot-password">I forgot my password</a>
 

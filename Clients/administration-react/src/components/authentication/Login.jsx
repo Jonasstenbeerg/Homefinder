8c8a4ff39eb8d1bf0cde9c84441c827f16dc96d3
Jonas <jonasstenbeerg@gmail.com>
2023-04-20 16:54:03 +0200

    added submit login method

--- Clients/administration-react/src/components/authentication/Login.jsx
+++ Clients/administration-react/src/components/authentication/Login.jsx
@@ -1,14 +1,47 @@
+import { useState } from "react";
+import { useSignIn } from "react-auth-kit";
+
 const Login = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const passwordChangeHandler = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const userNameChangeHandler = (e) => {
+    setUserName(e.target.value)
+  }
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+
+    const url = `${process.env.REACT_APP_API_BASEURL}auth/login`
+    
+    const user = {
+      userName: userName,
+      password: password,
+    };
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+  }
+
   return (
     
-    <form action="/login" method="post" className="form-container">
+    <form onSubmit={handleLogin} className="form-container">
       <section className="form-props-area">
         <h1 className="form-heading">Sign in</h1>
         <label className="form-label" htmlFor="form-username">Username:</label>
-        <input className="form-input" placeholder="Username" type="text" id="form-username" name="username" tocomplete="off" required/>
+        <input value={password} className="form-input" onChange={passwordChangeHandler} placeholder="Username" type="text" id="form-username" name="username" tocomplete="off" required/>
         
         <label className="form-label" htmlFor="form-password">Password:</label>
-        <input className="form-input" placeholder="Password" type="password" id="form-password" name="password" tocomplete="off" required/>
+        <input value={userName} className="form-input" onChange={userNameChangeHandler} placeholder="Password" type="password" id="form-password" name="password" tocomplete="off" required/>
         
         <a href="/forgot-password" id="form-forgot-password">I forgot my password</a>
 

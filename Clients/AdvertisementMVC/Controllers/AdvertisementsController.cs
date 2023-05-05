dcf52366baab8ad99e2c6a0dfa23423b79d77523
MadeleineGH <89448074+MadeleineGH@users.noreply.github.com>
2023-05-05 14:12:30 +0200

    Search filter fix

--- Clients/AdvertisementMVC/Controllers/AdvertisementsController.cs
+++ Clients/AdvertisementMVC/Controllers/AdvertisementsController.cs
@@ -23,52 +23,52 @@ namespace AdvertisementMVC.Controllers
 		{
 			try
 			{
-				// var advertisements = await _advertisementService.ListAllAdvertisementsAsync();
+				var advertisements = await _advertisementService.ListAllAdvertisementsAsync();
 
-				// return View("Index", advertisements);
-				var ads = new List<AdvertisementViewModel>
-    		{
-        	new AdvertisementViewModel 
-					{ 
-						Id = 0,
-    				ListPrice = 2400000,
-						LeaseType = "Villa",
-						LeaseTypeIconUrl = "../images/lease-type-house.png", 
-						PropertyType = "Mark",
-   					Area =  180,
- 						City =  "Gävle",
- 						StreetName = "Majorsgatan", 
-   					StreetNumber = 13,
-						ImageUrl = "../images/ad-1.jpg"
-						},
-        	new AdvertisementViewModel 
-					{ 
-						Id = 1,
-						ListPrice = 3700000,
-						LeaseType = "Lägenhet",
-						LeaseTypeIconUrl = "../images/lease-type-apartment.png", 
-						PropertyType = "Mark",
-   					Area =  75,
- 						City =  "Göteborg",
- 						StreetName = "Kontorsgatan", 
-   					StreetNumber = 15, 
-						ImageUrl = "../images/ad-2.jpg"
-						},
-        	new AdvertisementViewModel 
-					{ 
-						Id = 2,
-						ListPrice = 1570000,
-						LeaseType = "Lägenhet",
-						LeaseTypeIconUrl = "../images/lease-type-apartment.png",  
-						PropertyType = "Mark",
-   					Area =  65,
- 						City =  "Malmö",
- 						StreetName = "Storgatan", 
-   					StreetNumber = 5, 
-						ImageUrl = "../images/ad-3.jpg"
-						}
-    		};
-				return View(ads);
+				return View("Index", advertisements);
+				// var ads = new List<AdvertisementViewModel>
+    		// {
+        // 	new AdvertisementViewModel 
+				// 	{ 
+				// 		Id = 0,
+    		// 		ListPrice = 2400000,
+				// 		LeaseType = "Villa",
+				// 		LeaseTypeIconUrl = "../images/lease-type-house.png", 
+				// 		PropertyType = "Mark",
+   			// 		Area =  180,
+ 				// 		City =  "Gävle",
+ 				// 		StreetName = "Majorsgatan", 
+   			// 		StreetNumber = 13,
+				// 		ImageUrl = "../images/ad-1.jpg"
+				// 		},
+        // 	new AdvertisementViewModel 
+				// 	{ 
+				// 		Id = 1,
+				// 		ListPrice = 3700000,
+				// 		LeaseType = "Lägenhet",
+				// 		LeaseTypeIconUrl = "../images/lease-type-apartment.png", 
+				// 		PropertyType = "Mark",
+   			// 		Area =  75,
+ 				// 		City =  "Göteborg",
+ 				// 		StreetName = "Kontorsgatan", 
+   			// 		StreetNumber = 15, 
+				// 		ImageUrl = "../images/ad-2.jpg"
+				// 		},
+        // 	new AdvertisementViewModel 
+				// 	{ 
+				// 		Id = 2,
+				// 		ListPrice = 1570000,
+				// 		LeaseType = "Lägenhet",
+				// 		LeaseTypeIconUrl = "../images/lease-type-apartment.png",  
+				// 		PropertyType = "Mark",
+   			// 		Area =  65,
+ 				// 		City =  "Malmö",
+ 				// 		StreetName = "Storgatan", 
+   			// 		StreetNumber = 5, 
+				// 		ImageUrl = "../images/ad-3.jpg"
+				// 		}
+    		// };
+				// return View(ads);
 
 			}
 			catch (System.Exception)

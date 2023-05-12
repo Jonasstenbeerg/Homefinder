a48875fb989dfbace3c17b6ba97ecc94109a923b
Jonas <jonasstenbeerg@gmail.com>
2023-05-12 09:55:58 +0200

    IntegrationTest structure implemented

---
+++ Tests/HomefinderAPI.IntegrationTests/Controllers/AdvertisementsControllerTests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HomefinderAPI.IntegrationTests.Controllers
+{
+	[TestClass]
+	public class AdvertisementsControllerTests : IntegrationTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Assert.IsTrue(true);
+		}
+	}
+}
---
+++ Tests/HomefinderAPI.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomefinderAPI.IntegrationTests
+{
+	public class IntegrationTest
+	{
+		
+	}
+}
